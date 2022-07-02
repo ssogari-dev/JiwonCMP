74357fc5d455f17d12dde965952d4887e365b730
황보현 <hwang0911_02@naver.com>
2022-07-02 14:17:02 +0000

    set

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import ChatPage from './components/ChatPage/ChatPage';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
+
+function App(){
+  return(
+    <Router>
+
+        <hr />
+
+        <Switch>
+          <Route exact path="/" component={ChatPage} />
+          <Route exact path="/" component={LoginPage} />
+          <Route exact path="/" component={RegisterPage} />
+        </Switch>
+    </Router>
+
   );
 }
 
