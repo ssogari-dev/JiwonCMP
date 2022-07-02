e05647f5b8f095015d395d5a9fa67d5c90301bbf
황보현 <hwang0911_02@naver.com>
2022-07-02 14:50:58 +0000

    setttt

--- chatapp/src/index.js
+++ chatapp/src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {Provider } from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './redux/reducer';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStoreWithMiddleware(Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__())}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
